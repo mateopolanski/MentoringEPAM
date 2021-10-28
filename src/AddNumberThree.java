04b09346d2fcdcb902800a37654d566b077f68aa
Mateusz <mateopolanski@gmail.com>
2021-10-28 08:49:21 +0200

    Update

--- src/AddNumberThree.java
+++ src/AddNumberThree.java
@@ -6,17 +6,18 @@ public class AddNumberThree {
         public static void main(String[] args) {
 
             int test = plusThree();
-            System.out.print(test);
+
         }
 
         public static int plusThree(){
 
+            int x = 3;
+
             Scanner sc = new Scanner( System.in );
             System.out.println("Enter a number: ");
-            int i = 2;
-            int j = sc.nextInt();
+            int a = sc.nextInt();
 
-            int result = i + j;
+            int result = a + x ;
             System.out.print ("Your Total number plus 3 is: "+ result);
             return result;
         }
